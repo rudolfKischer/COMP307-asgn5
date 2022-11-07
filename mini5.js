5518396bea4b7b61e591b1c2a1f4f0d6fd3c99c9
rudolfKischer <rudikischer@gmail.com>
2022-11-07 14:21:28 -0500

    Create-basic-content-and-page-generation-and-navigation

---
+++ mini5.js
@@ -0,0 +1,44 @@
+
+console.log("Hello");
+
+//nav
+
+
+// function navBar(){
+//     let navPages = document.getElementById('nav').children;
+//     for (let i=0; i<navPages.length; i++) {
+//         page = navPages[i];
+//         page.setAttribute("onclick", "playAudio("+i+");")
+//         page.onClick = loadPage(page.innerHTML.toLowerCase()+","+page+")";
+//     }
+// }
+
+function loadPage(page, elementid){
+    let navPages = document.getElementById('nav').children;
+    for (let page of navPages) {
+        if(page.id == elementid){
+            page.classList.add('activeNavPage');
+        }else{
+            page.classList.remove('activeNavPage');
+        }
+       
+    }
+    
+    let path = './matter/mini5'+page+'.txt';
+    loadDoc(path, 'content');
+}
+
+function loadDoc(docName, elementId) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById(elementId).innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", docName, true);
+    xhttp.send();
+}
+
+function load(){
+    loadDoc('./matter/mini5home.txt','content');
+}
--- mini5.php
+++ mini5.php
@@ -1,8 +1,31 @@
 <html>
  <head>
+ <link rel="stylesheet" href="style.css">
   <title>PHP Test</title>
  </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?>
+ <body onload='load()'>
+ <?php 
+ $header = './matter/mini5header.txt';
+ $content = './matter/mini5content.txt';
+ $home = './matter/mini5home.txt';
+ $hobbies = './matter/mini5hobbies.txt';
+ $footer = './matter/mini5footer.txt';
+ 
+
+ display($header);
+ display($content);
+ display($footer);
+ 
+
+function display($path) {
+    $file = fopen($path,"r");
+    while(!feof($file)) {
+        $line = fgets($file);
+        echo $line;
+    }
+    fclose($file);
+}
+ ?>
+ <script type="text/javascript" src="mini5.js"></script>
  </body>
 </html>
